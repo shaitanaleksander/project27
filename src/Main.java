28a7587d3d1de49138e58de0a537b630697d0f6e
shaitan <alexandr.shaitan@diymaxeda.com>
2023-03-03 19:12:01 +0100

    changed message

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        sayHi("Bogdan");
+        sayHi("Alex");
 
         }
 
         public static void sayHi(String name) {
-            System.out.println("hello " + name);
+            System.out.println("go home  " + name);
         }
     }
 
